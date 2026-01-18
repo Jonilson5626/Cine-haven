da8ac6901669bfbe249feafc6e67576f2a029ca5
Jonilson5626 <jonilsonalves80@gmail.com>
2026-01-18 18:03:02 -0300

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,44 @@
+const MoviesData = [
+    {
+        id: 1, title: "Duna: Parte 2", ano: 2024, genero: ["Ficção", "Aventura"], nota: "8.8", destaque: true,
+        poster: "https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+        banner: "https://image.tmdb.org/t/p/original/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg",
+        sinopse: "Paul Atreides se une aos Fremen para se vingar dos conspiradores."
+    },
+    {
+        id: 2, title: "Godzilla e Kong", ano: 2024, genero: ["Ação", "Ficção"], nota: "7.0", destaque: true,
+        poster: "https://image.tmdb.org/t/p/w500/tMefBSflR6PGQLv7WvFPpKLZkyk.jpg",
+        banner: "https://image.tmdb.org/t/p/original/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg",
+        sinopse: "Uma nova ameaça colossal espreita nas profundezas."
+    },
+    {
+        id: 3, title: "Oppenheimer", ano: 2023, genero: ["Drama"], nota: "8.1", destaque: true,
+        poster: "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg", 
+        banner: "https://image.tmdb.org/t/p/original/rLb2cs785pePbIKYQz18IvuNPJh.jpg",
+        sinopse: "A história do físico que liderou o Projeto Manhattan."
+    },
+    {
+        id: 4, title: "Kung Fu Panda 4", ano: 2024, genero: ["Animação"], nota: "7.6", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg", banner: "", sinopse: "Po treina um novo dragão."
+    },
+    {
+        id: 5, title: "Guerra Civil", ano: 2024, genero: ["Ação"], nota: "7.4", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/63QG25m4721aB0b54rK69q9.jpg", banner: "", sinopse: "Jornalistas na guerra."
+    },
+    {
+        id: 6, title: "Homem-Aranha", ano: 2023, genero: ["Animação"], nota: "8.4", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg", banner: "", sinopse: "Miles no multiverso."
+    },
+    {
+        id: 7, title: "Planeta dos Macacos", ano: 2024, genero: ["Ação"], nota: "7.1", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/gKkl37BQuKTanygYQG1pyYgLVgf.jpg", banner: "", sinopse: "O reinado de Proximus."
+    },
+    {
+        id: 8, title: "A Freira 2", ano: 2023, genero: ["Terror"], nota: "6.0", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/5gzzkR7y3zdL8NOVDkYe83tO26l.jpg", banner: "", sinopse: "O mal retorna."
+    },
+    {
+        id: 9, title: "John Wick 4", ano: 2023, genero: ["Ação"], nota: "7.8", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/r2J02Z2OpNTctfOSN1Ydgii51I3.jpg", banner: "", sinopse: "Baba Yaga contra a Cúpula."
+    }
+];
