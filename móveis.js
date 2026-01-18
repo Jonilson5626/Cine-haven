33a245583737c19f767bb810b86ed120fe4ed321
Jonilson5626 <jonilsonalves80@gmail.com>
2026-01-18 16:22:37 -0300

    Add MoviesData array with movie details

---
+++ móveis.js
@@ -0,0 +1,78 @@
+const MoviesData = [
+    {
+        id: 1, title: "Duna: Parte 2", ano: 2024, genero: ["Ficção", "Aventura"], nota: "8.8", destaque: true,
+        poster: "https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+        banner: "https://image.tmdb.org/t/p/original/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg",
+        sinopse: "Paul Atreides se une aos Fremen para se vingar dos conspiradores que destruíram sua família.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 2, titulo: "Godzilla e Kong", ano: 2024, genero: ["Ação", "Ficção"], nota: "7.0", destaque: true,
+        poster: "https://image.tmdb.org/t/p/w500/tMefBSflR6PGQLv7WvFPpKLZkyk.jpg",
+        banner: "https://image.tmdb.org/t/p/original/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg",
+        sinopse: "Uma nova ameaça colossal espreita nas profundezas do planeta.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 3, titulo: "Kung Fu Panda 4", ano: 2024, genero: ["Animação"], nota: "7.6", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg", banner: "",
+        sinopse: "Po é escolhido para se tornar o Líder Espiritual do Vale da Paz.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 4, titulo: "Oppenheimer", ano: 2023, genero: ["Drama"], nota: "8.1", destaque: true,
+        poster: "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg", 
+        banner: "https://image.tmdb.org/t/p/original/rLb2cs785pePbIKYQz18IvuNPJh.jpg",
+        sinopse: "A história do físico que liderou o Projeto Manhattan.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 5, titulo: "Homem-Aranha: Aranhaverso 2", ano: 2023, genero: ["Animação", "Ação"], nota: "8.4", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg", banner: "",
+        sinopse: "Miles Morales viaja pelo multiverso.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 6, titulo: "Guerra Civil", ano: 2024, genero: ["Ação"], nota: "7.4", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/63QG25m4721aB0b54rK69q9.jpg", banner: "",
+        sinopse: "Jornalistas viajam pelos EUA durante uma guerra civil.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 7, titulo: "Planeta dos Macacos", ano: 2024, genero: ["Ação", "Ficção"], nota: "7.1", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/gKkl37BQuKTanygYQG1pyYgLVgf.jpg", banner: "",
+        sinopse: "Novo reinado dos macacos.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 8, titulo: "A Freira 2", ano: 2023, genero: ["Terror"], nota: "6.0", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/5gzzkR7y3zdL8NOVDkYe83tO26l.jpg", banner: "",
+        sinopse: "O mal se espalha pela França.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 9, titulo: "John Wick 4", ano: 2023, genero: ["Ação"], nota: "7.8", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/r2J02Z2OpNTctfOSN1Ydgii51I3.jpg", banner: "",
+        sinopse: "John Wick descobre um caminho para derrotar a Alta Cúpula.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 10, titulo: "Fallout", ano: 2024, genero: ["Série", "Ficção"], nota: "8.5", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/8cO8843AT3435g71t69R733u.jpg", banner: "",
+        sinopse: "O fim é apenas o começo.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 11, titulo: "Avatar 2", ano: 2022, genero: ["Ficção"], nota: "7.7", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", banner: "",
+        sinopse: "O caminho da água conecta todas as coisas.",
+        linkDownload: "#", linkAssistir: "#"
+    },
+    {
+        id: 12, titulo: "Guardiões da Galáxia 3", ano: 2023, genero: ["Ação"], nota: "8.0", destaque: false,
+        poster: "https://image.tmdb.org/t/p/w500/r2J02Z2OpNTctfOSN1Ydgii51I3.jpg", banner: "",
+        sinopse: "A equipe deve proteger um dos seus.",
+        linkDownload: "#", linkAssistir: "#"
+    }
+];
+          
