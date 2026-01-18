2eb829c9d83553e97285b49c520aee7e1c660161
Jonilson5626 <jonilsonalves80@gmail.com>
2026-01-18 18:00:58 -0300

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Estas variáveis virão do seu arquivo .env.local
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
